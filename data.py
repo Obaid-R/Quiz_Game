4d0930642c4b2a5843a2feae7d20bcdce0ab9961
Obaid-ur-Rehman <119891778+Obaid-R@users.noreply.github.com>
2023-09-11 07:17:14 -0600

    Add files via upload

---
+++ data.py
@@ -0,0 +1,83 @@
+# To generate different/more questions, go here: https://opentdb.com/.
+
+question_data = [
+    {
+    "category": "General Knowledge", 
+    "type": "boolean", 
+    "difficulty": "hard", 
+    "question": "This is the correct spelling of 'Supercalifragilisticexpialidocious'.", 
+    "correct_answer": "True", 
+    "incorrect_answers": ["False"]
+    }, 
+    {
+    "category": "General Knowledge", 
+    "type": "boolean", 
+    "difficulty": "medium", 
+    "question": "An eggplant is a vegetable.", 
+    "correct_answer": "False", 
+    "incorrect_answers": ["True"]
+    }, 
+    {
+    "category": "General Knowledge", 
+    "type": "boolean", 
+    "difficulty": "hard", 
+    "question": "'Number 16 Bus Shelter' was a child's name that was approved by the New Zealand government.", 
+    "correct_answer": "True", 
+    "incorrect_answers": ["False"]
+    }, 
+    {
+    "category": "General Knowledge", 
+    "type": "boolean", 
+    "difficulty": "easy", 
+    "question": "In 2010, Twitter and the United States Library of Congress partnered together to archive every tweet by American citizens.", 
+    "correct_answer": "True", 
+    "incorrect_answers": ["False"]
+    }, 
+    {
+    "category": "General Knowledge", 
+    "type": "boolean", 
+    "difficulty": "medium", 
+    "question": "Kissing someone for one minute burns about 2 calories.", 
+    "correct_answer": "True", 
+    "incorrect_answers": ["False"]
+    }, 
+    {
+    "category": "General Knowledge", 
+    "type": "boolean", 
+    "difficulty": "easy", 
+    "question": "The Lego Group was founded in 1932.", 
+    "correct_answer": "True", 
+    "incorrect_answers": ["False"]
+    }, 
+    {
+    "category": "General Knowledge", 
+    "type": "boolean", 
+    "difficulty": "easy", 
+    "question": "One of Donald Trump's 2016 Presidential Campaign promises was to build a border wall between the United States and Mexico.", "correct_answer": "True", 
+    "incorrect_answers": ["False"]
+    }, 
+    {
+    "category": "General Knowledge", 
+    "type": "boolean", 
+    "difficulty": "easy", 
+    "question": "Dihydrogen Monoxide was banned due to health risks after being discovered in 1983 inside swimming pools and drinking water.", 
+    "correct_answer": "False", 
+    "incorrect_answers": ["True"]
+    }, 
+    {
+    "category": "General Knowledge", 
+    "type": "boolean", 
+    "difficulty": "medium", 
+    "question": "'Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo.' is a grammatically correct sentence.", 
+    "correct_answer": "True", 
+    "incorrect_answers": ["False"]
+    }, 
+    {
+    "category": "General Knowledge", 
+    "type": "boolean", 
+    "difficulty": "easy", 
+    "question": "The mitochondria is the powerhouse of the cell.", 
+    "correct_answer": "True", 
+    "incorrect_answers": ["False"]
+    },
+    ]
